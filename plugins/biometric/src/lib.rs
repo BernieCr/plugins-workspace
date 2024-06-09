8f958fff1a73f5821fafa5c34b9787accc87b9f4
johnnynomad <hello@bernhardcaspar.de>
2024-06-09 18:19:30 +0545

    Make biometric plugin compile on desktop

--- plugins/biometric/src/lib.rs
+++ plugins/biometric/src/lib.rs
@@ -24,29 +24,29 @@ const PLUGIN_IDENTIFIER: &str = "app.tauri.biometric";
 tauri::ios_plugin_binding!(init_plugin_biometric);
 
 /// Access to the biometric APIs.
-#[cfg(mobile)] {
-	pub struct Biometric<R: Runtime>(PluginHandle<R>);
+#[cfg(mobile)]
+pub struct Biometric<R: Runtime>(PluginHandle<R>);
 
-	impl<R: Runtime> Biometric<R> {
-        pub fn status(&self) -> crate::Result<Status> {
-            self.0.run_mobile_plugin("status", ()).map_err(Into::into)
-        }
+[cfg(not(mobile))] {
+pub struct Biometric();
 
-        pub fn authenticate(&self, reason: String, options: AuthOptions) -> crate::Result<()> {
-            self.0
-	            .run_mobile_plugin("authenticate", AuthenticatePayload { reason, options })
-	            .map_err(Into::into)
-        }
+#[cfg(mobile)]
+impl<R: Runtime> Biometric<R> {
+    pub fn status(&self) -> crate::Result<Status> {
+        self.0.run_mobile_plugin("status", ()).map_err(Into::into)
+    }
+
+    pub fn authenticate(&self, reason: String, options: AuthOptions) -> crate::Result<()> {
+        self.0
+            .run_mobile_plugin("authenticate", AuthenticatePayload { reason, options })
+            .map_err(Into::into)
     }
 }
 
-#[cfg(not(mobile))] {
-	pub struct Biometric();
-
-	impl Biometric<> {
-        pub fn authenticate(&self, reason: String, options: AuthOptions) -> crate::Result<()> {
-           Ok(())
-        }
+#[cfg(not(mobile))]
+impl Biometric<> {
+    pub fn authenticate(&self, reason: String, options: AuthOptions) -> crate::Result<()> {
+       Ok(())
     }
 }
 
